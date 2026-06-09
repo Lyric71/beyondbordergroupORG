e7f9bf936e97bfc31b88103c29a69abbe337a6f8
Lyric71 <cyril.drouin@outlook.com>
2026-06-09 11:26:37 +0200

    0609-1126

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://bearingbridge.org',
   output: 'static',
   adapter: vercel(),
   server: {
